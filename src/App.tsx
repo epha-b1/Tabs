0db608430a6ef2055384c5781695e678173b6db9
Ephrem B <ephremyonatan27@gmail.com>
2024-10-16 14:54:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Profile from "./components/Profile";
+import SideBar from "./components/SideBar";
+
+export default function App() {
+  return (
+    <div>
+      <SideBar />
+      <Profile />
+    </div>
+  );
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { FaCamera } from 'react-icons/fa';
+
+const Profile = () => {
+  const [bannerUrl, setBannerUrl] = useState("https://placehold.co/1500x400");
+  const [profileUrl, setProfileUrl] = useState("https://placehold.co/100");
+
+  // Handlers for image changes
+  const handleBannerChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // The line of code is used in JavaScript to create a temporary URL that points to a Blob or File object.
+      setBannerUrl(URL.createObjectURL(file));
+    }
+  };
+  const handleProfileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setProfileUrl(URL.createObjectURL(file));
+    }
+  };
+  
+  return (
+    <div className='relative w-[94%] ml-[5rem]'>
+      <div className="relative">
+        <img src={bannerUrl} alt="background image" className='w-full h-60 object-cover' />
+        <button className='absolute top-2 right-2 bg-gray-800 text-white p-2 rounded-full hover:bg-gray-600'>
+        <label htmlFor="banner-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            id="banner-upload"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleBannerChange}
+          />
+        </button>
+      </div>
+
+      {/* Channel Logo */}
+      <div className="flex items-center ml-4 mt-[2rem] ">
+        <img
+          src={profileUrl}
+          alt="Channel Logo"
+          className="w-40 h-40  object-cover rounded-full border-4 border-white relative"
+        />
+
+        <button className="absolute  ml-[3.6rem] mt-[10rem] bg-gray-800 text-white p-2 rounded-full hover:bg-gray-600">
+          <label htmlFor="profile-upload" className="cursor-pointer">
+            <FaCamera size={24} />
+          </label>
+          <input
+            id="profile-upload"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleProfileChange}
+          />
+        </button>
+
+        {/* Channel Details */}
+        <div className="ml-4 mt-4">
+          <h1 className="text-2xl font-bold">HuXn WebDev</h1>
+          <p>1M views</p>
+          <p className="mt-2">
+            This is a short description of the YouTube channel. It gives an
+            overview of the content and what viewers can expect.
+          </p>
+          <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
